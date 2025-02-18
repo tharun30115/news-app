191085c335f11c4a95f0c5f70a8e20c40665a836
Tharun <tharungowda30115@gmail.com>
2025-02-18 22:49:45 +0530

    todo app react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Todo from "./Components/Todo"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Todo/>
+    </div>
   )
 }
 
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,17 @@
+import './CSS/Todo.css'
+const Todo = () => {
+  return (
+    <div className='todo'>
+        <div className="todo-header">To-Do List</div>
+        <div className="todo-add">
+            <input type="text" placeholder='Add your task' className='todo-input' />
+            <div className="todo-add-btn">Add</div>
+        </div>
+        <div className="todo-list">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Todo
