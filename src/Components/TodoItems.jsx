e2453b8b324fecb440996a7ef962e910174809bf
Tharun <tharungowda30115@gmail.com>
2025-02-18 23:25:17 +0530

    todo app react

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,8 +1,17 @@
 import './CSS/TodoItems.css'
-const TodoItems = () => {
-  return (
-    <div>
+import tick from './Assets/tick.png'
+import cross from './Assets/cross.png'
+import not_tick from './Assets/not_tick.png'
 
+const TodoItems = ({no,display,text}) => {
+  return (
+    <div className='todoitems'>
+        <div className="todoitems-container">
+            <img src={not_tick} alt="" />
+            <img src={tick} alt="" />
+            <div className="todoitems-text">{text}</div>
+        </div>
+        <img src={cross} alt="" />
     </div>
   )
 }
